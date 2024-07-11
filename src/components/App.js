63e9098abc8beb3b7d5a92098d3f3bef9e84fbe0
Admin <purohitparthiv@gmail.com>
2024-07-12 00:09:11 +0530

    lets see

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
 import React from "react";
 import './../styles/App.css';
+var ans = "";
+const inputElem = document.getElementById('input');
+inputElem.addEventListener('input', handleSearchInput);
+function handleSearchInput(event){
+    const val =  event.target.value;
+    ans = val;
 
+}
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <input class="custom-placeholder" id="input" type="text"></input>
+        if{ans != ""}
+        {
+          <p>Hello {ans}!</p>
+        }
     </div>
   )
 }
